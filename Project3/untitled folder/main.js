6307d645ea7591dfdbed924eb3412783caa85e53
erinjohnstonn <Erin@Erins-MacBook-Pro.local>
2016-04-26 09:09:25 -0400

    project

--- Project3/untitled folder/main.js
+++ Project3/untitled folder/main.js
@@ -22,14 +22,58 @@ $(document).ready(function(){
     });   
 });
 
-/*
+
+
+
+
+
+
 
 var messageContainer = document.querySelectorAll('.messages')[0];
 var addLink = document.querySelectorAll('.add-link')[0];
+var quoteValues = {
+  0: 'quote1',
+  1: 'quote2',
+  2: 'quote3',
+  3: 'quote4',
+  4: 'quote5',  
+};
 
+var imageValues = {
+  5: 'image1',
+  6: 'image2',
+  7: 'image3',
+  8: 'image4',
+  9: 'image5',  
+};
 
 Chat.onMessage(function (data) {
-  var message = document.createElement('div');
+  var message = $('<div></div>');
+  var quote = $("." + quoteValues[data.value]);
+  alert(quote.html());
+  $(message).addClass(quoteValues[data.value]);
+  if (data.sender == username) {
+     $(message).addClass('from-current-user');
+  }
+
+  $(messageLabel).html(data.sender);
+  $(message).append(messageLabel);
+  $('.message-container').append(message);
+});
+Chat.onUserCountChange(function (currentCount) {
+    if (currentCount > 20) {
+    currentCount = 20;
+  }
+
+
+
+
+
+
+
+
+
+
   message.className = 'message';
   message.innerHTML += 'From: ' + data.sender + '<br>';
   message.innerHTML += 'Type: ' + data.type + '<br>';
@@ -42,4 +86,9 @@ Chat.onUserCountChange(function (newCount) {
   userCountElement.innerHTML = newCount;
 });
 
-*/
+
+
+
+
+
+
